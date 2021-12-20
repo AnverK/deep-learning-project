d14e1227732f00a5bae92d0183bb24cc9cc95af6
Orhan <osaeedi@student.ethz.ch>
2021-12-20 13:43:28 +0100

    some lightning changes in adv_gan implementation

--- adv_gan_lightning/adv_gan.py
+++ adv_gan_lightning/adv_gan.py
@@ -43,13 +43,6 @@ class AdvGAN(LightningModule):
         self.output_nc = output_nc
         self.box_min = box_min
         self.box_max = box_max
-        
-        # Losses
-        self.lossD = 100000
-        self.lossG_fake = 100000
-        self.loss_perturb = 100000
-        self.loss_adv = 100000
-        self.lossG = 100000
 
         # networks
         self.gen_input_nc = image_nc
@@ -66,61 +59,73 @@ class AdvGAN(LightningModule):
     def forward(self, z):
         return self.generator(z)
 
-    def adversarial_loss(self, y_hat, y):
+    def loss(self, y_hat, y):
         return F.mse_loss(y_hat, y)
 
     def training_step(self, batch, batch_idx, optimizer_idx):
-        x, labels = batch
+        imgs, labels = batch
 
-        perturbation = self.generator(x)
+        perturbation = self.generator(imgs)
         # add a clipping trick
-        adv_images = torch.clamp(perturbation, -0.3, 0.3) + x
+        adv_images = torch.clamp(perturbation, -0.3, 0.3) + imgs
         adv_images = torch.clamp(adv_images, self.box_min, self.box_max)
 
         # optimize G
         if optimizer_idx == 0:
             # cal G's loss in GAN
+            # ground truth result (ie: all fake)
+            # put on GPU because we created this tensor inside training_loop
             pred_fake = self.discriminator(adv_images)
-            self.lossG_fake = self.adversarial_loss(pred_fake, torch.ones_like(pred_fake, device=self.device))
-            self.log("Generator loss", self.lossG_fake)
+            valid = torch.ones_like(pred_fake)
+            valid = valid.type_as(imgs)
+            lossG_fake = self.loss(pred_fake, valid)
+            self.log("Generator loss", lossG_fake)
 
             # calculate perturbation norm
-            C = 0.1
-            self.loss_perturb = torch.mean(torch.norm(perturbation.view(perturbation.shape[0], -1), 2, dim=1))
-            self.log("Perturbation loss", self.loss_perturb)
+            # C = 0.1
+            norm_perturb = torch.norm(perturbation.view(perturbation.shape[0], -1), 2, dim=1)
+            loss_perturb = torch.mean(norm_perturb)
+            self.log("Perturbation loss", loss_perturb)
             # loss_perturb = torch.max(loss_perturb - C, torch.zeros(1, device=self.device))
 
             # cal adv loss
-            logits_model = self.model(adv_images)
-            probs_model = F.softmax(logits_model, dim=1)
-            onehot_labels = torch.eye(self.model_num_labels, device=self.device)[labels]
-
+            preds = self.model(adv_images)
+            probs = F.softmax(preds, dim=1)
+            onehot_labels = torch.eye(self.model_num_labels)[labels]
+            onehot_labels = onehot_labels.type_as(onehot_labels)
             # C&W loss function
-            real = torch.sum(onehot_labels * probs_model, dim=1)
-            other, _ = torch.max((1 - onehot_labels) * probs_model - onehot_labels * 10000, dim=1)
+            real = torch.sum(onehot_labels * probs, dim=1)
+            other, _ = torch.max((1 - onehot_labels) * probs - onehot_labels * 10000, dim=1)
             zeros = torch.zeros_like(other)
             loss_adv = torch.max(real - other, zeros)
-            self.loss_adv = torch.sum(loss_adv)
-            self.log("Adversary loss", self.loss_adv)
+            loss_adv = torch.sum(loss_adv)
+            self.log("Adversary loss", loss_adv)
 
             adv_lambda = 10
             pert_lambda = 1
-            self.lossG = adv_lambda * self.lossG_fake + self.loss_adv + pert_lambda * self.loss_perturb
-            self.log("Generator loss Overall", self.lossG)
+            lossG = adv_lambda * lossG_fake + loss_adv + pert_lambda * loss_perturb
+            self.log("Generator loss Overall", lossG)
             tqdm_dict = {"lossG": self.lossG}
-            output = OrderedDict({"loss": self.lossG, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            output = OrderedDict({"loss": lossG, "progress_bar": tqdm_dict, "log": tqdm_dict})
             return output
 
         if optimizer_idx == 1:
-            pred_real = self.discriminator(x)
-            lossD_real = self.adversarial_loss(pred_real, torch.ones_like(pred_real, device=self.device))
+            # how well can it label as real?
+            pred_real = self.discriminator(imgs)
+            valid = torch.ones_like(pred_real)
+            valid = valid.type_as(imgs)
+            lossD_real = self.loss(pred_real, valid)
 
+            # how well can it label as fake?
             pred_fake = self.discriminator(adv_images.detach())
-            lossD_fake = self.adversarial_loss(pred_fake, torch.zeros_like(pred_fake, device=self.device))
-            self.lossD = (lossD_fake + lossD_real)/2
-            self.log("Discriminator loss", self.lossD)
-            tqdm_dict = {"lossD": self.lossD}
-            output = OrderedDict({"loss": self.lossD, "progress_bar": tqdm_dict, "log": tqdm_dict})
+            fake = torch.zeros_like(pred_fake)
+            fake = fake.type_as(imgs)
+            lossD_fake = self.loss(pred_fake, fake)
+
+            lossD = (lossD_fake + lossD_real) / 2
+            self.log("Discriminator loss", lossD)
+            tqdm_dict = {"lossD": lossD}
+            output = OrderedDict({"loss": lossD, "progress_bar": tqdm_dict, "log": tqdm_dict})
             return output
 
     def configure_optimizers(self):
@@ -137,10 +142,3 @@ class AdvGAN(LightningModule):
             self.lr = 0.0001
         if self.current_epoch == 80:
             self.lr = 0.00001
-
-        print("\nEpoch Nr. %d:\nDiscriminator Loss: %.3f,\nGenerator Loss: %.3f,\
-                     \nPerturbation Loss: %.3f,\nAdversary Loss: %.3f,\
-                    \nOverall Generator Loss:  %.3f \n" %
-              (self.current_epoch, self.lossD, self.lossG_fake,
-               self.loss_perturb, self.loss_adv, self.lossG))
-
