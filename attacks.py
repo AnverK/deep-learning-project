2c4a182f01918f92e5c5abca323973dbf385f5e0
Orhan <osaeedi@student.ethz.ch>
2022-01-01 22:21:34 +0100

    add clip_min and clip_max

--- attacks.py
+++ attacks.py
@@ -14,11 +14,15 @@ class FGSM(nn.Module):
     def __init__(self,
                  eps=0.3,
                  norm=np.inf,
+                 clip_min=0,
+                 clip_max=1,
                  target_model_dir='../target_models/pytorch/adv_trained.ckpt'):
         super(FGSM, self).__init__()
 
         self.eps = eps
         self.norm = norm
+        self.clip_min = clip_min
+        self.clip_max = clip_max
 
         self.target_model = TargetModel()
         self.target_model.load_state_dict(torch.load(target_model_dir))
@@ -26,9 +30,54 @@ class FGSM(nn.Module):
         self.target_model.eval()
 
     def forward(self, imgs):
-        imgs_fgsm = fast_gradient_method(self.target_model, imgs, self.eps, self.norm)
+        imgs_fgsm = fast_gradient_method(
+            self.target_model,
+            imgs,
+            self.eps,
+            self.norm,
+            self.clip_min,
+            self.clip_max
+        )
 
         return imgs_fgsm
+
+
+class PGD(nn.Module):
+    def __init__(self,
+                 eps=0.3,
+                 eps_iter=0.01,
+                 nb_iter=40,
+                 norm=np.inf,
+                 clip_min=0,
+                 clip_max=1,
+                 target_model_dir='../target_models/pytorch/adv_trained.ckpt'):
+        super(PGD, self).__init__()
+
+        self.eps = eps
+        self.eps_iter = eps_iter
+        self.nb_iter = nb_iter
+        self.norm = norm
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+
+        self.target_model = TargetModel()
+        self.target_model.load_state_dict(torch.load(target_model_dir))
+        self.target_model.freeze()
+        self.target_model.eval()
+
+    def forward(self, imgs):
+        imgs_pgd = projected_gradient_descent(
+            self.target_model,
+            imgs,
+            self.eps,
+            self.eps_iter,
+            self.nb_iter,
+            self.norm,
+            self.clip_min,
+            self.clip_max
+        )
+
+        return imgs_pgd
 
 
 class CW_L2(nn.Module):
@@ -48,28 +97,3 @@ class CW_L2(nn.Module):
         imgs_cw_l2 = carlini_wagner_l2(self.target_model, imgs, self.n_classes)
 
         return imgs_cw_l2
-
-
-class PGD(nn.Module):
-    def __init__(self,
-                 eps=0.3,
-                 eps_iter=0.01,
-                 nb_iter=40,
-                 norm=np.inf,
-                 target_model_dir='../target_models/pytorch/adv_trained.ckpt'):
-        super(PGD, self).__init__()
-
-        self.eps = eps
-        self.eps_iter = eps_iter
-        self.nb_iter = nb_iter
-        self.norm = norm
-
-        self.target_model = TargetModel()
-        self.target_model.load_state_dict(torch.load(target_model_dir))
-        self.target_model.freeze()
-        self.target_model.eval()
-
-    def forward(self, imgs):
-        imgs_pgd = projected_gradient_descent(self.target_model, imgs, self.eps, self.eps_iter, self.nb_iter, self.norm)
-
-        return imgs_pgd
