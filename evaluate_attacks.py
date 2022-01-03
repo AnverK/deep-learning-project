a910fc3b3d358edbcf8863258bfda149941408e7
Orhan <osaeedi@student.ethz.ch>
2022-01-03 10:45:07 +0100

    script adjusted

--- evaluate_attacks.py
+++ evaluate_attacks.py
@@ -5,13 +5,15 @@ adv_models = ['fgsm', 'adv_gan', 'pgd']
 
 for attack in attacks:
     if attack == 'adv_gan':
-        distill_modes = [False, True]
+        distill_modes = ['', '--attack-is-distilled']
     else:
-        distill_modes = [False]
+        distill_modes = ['']
 
-    for distilled in distill_modes:
+    for distill_mode in distill_modes:
         for adv_model in adv_models:
-            os.system(f'python evaluate_attack.py --attack {attack} --adv-model {adv_model} --attack-is-distilled {distilled}')
+            os.system(f'python evaluate_attack.py '
+                      f'--attack {attack} --adv-model {adv_model} {distill_mode}')
 
-            if adv_model == 'adv_gan': 
-                os.system(f'python evaluate_attack.py --attack {attack} --adv-model {adv_model} --attack-is-distilled {distilled} --defense-is-distilled {True}')
+            if adv_model == 'adv_gan':
+                os.system(f'python evaluate_attack.py '
+                          f'--attack {attack} --adv-model {adv_model} {distill_mode} --defense-is-distilled')
