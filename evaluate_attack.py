cce131606ae9c37366d6ac76eb35ddf1328aa170
akhusainov <akhusainov@student.ethz.ch>
2022-01-03 14:37:03 +0300

    Refactor evaluate script

--- evaluate_attack.py
+++ evaluate_attack.py
@@ -7,7 +7,7 @@ from torchvision import datasets
 from config import Config
 from create_paths import CreatePaths
 from models.adv_gan.adv_gan import AdvGAN
-from models.adv_gan.adv_gan_reverse import AdvGAN as AdvGANReverse
+# from models.adv_gan.adv_gan_reverse import AdvGAN as AdvGANReverse
 from models.ape_gan.ape_gan import ApeGan
 from models.target_models.target_model import TargetModel
 from attacks import FGSM, PGD
@@ -16,6 +16,8 @@ import pytorch_lightning as pl
 
 pl.seed_everything(51)
 
+ROBUST_MODEL_PATH = f'{Config.LOGS_PATH}/{Config.TARGET_MODEL_FOLDER}/converted_secret/model.ckpt'
+
 
 def check_distance(X, X_adv, eps=0.3):
     norm = torch.max(torch.abs(X - X_adv))
@@ -23,112 +25,41 @@ def check_distance(X, X_adv, eps=0.3):
     return norm <= eps + 1e-5
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--adv-model", type=str, default='adv_gan')
-    parser.add_argument("--attack", type=str, default='')
-    parser.add_argument("--no-eval-defense", default=False, action='store_true')
-    parser.add_argument("--attack-is-distilled", default=False, action='store_true')
-    parser.add_argument("--defense-is-distilled", default=False, action='store_true')
-    
-    parser.add_argument("--def-model", type=str, default='ape_gan')
-    parser.add_argument("--def_attack-is-distilled", default=False, action='store_true')
+def load_attack(adv_model_type, target_model_folder=None, adv_model_folder=None):
+    if adv_model_type == 'adv_gan':
+        adv_model_path = f'{adv_model_folder}/{Config.ADV_GAN_CKPT}'
 
-    # currently not used TODO
-    parser.add_argument("--dataset", type=str, default='mnist')
-    parser.add_argument("--eps", type=float, default=0.3)
-    args = parser.parse_args()
-
-    if args.attack == '':
-        args.attack = args.adv_model
-
-    PathCreator = CreatePaths(args.adv_model, is_distilled=args.defense_is_distilled)
-    TARGET_MODEL_PATH, ADV_MODEL_FOLDER, DEFENSE_MODEL_FOLDER = PathCreator.create_paths()
-
-    ROBUST_MODEL_PATH = f'{Config.LOGS_PATH}/{Config.TARGET_MODEL_FOLDER}/converted_secret/model.ckpt'
-
-    if args.adv_model == 'adv_gan':
-        ADV_MODEL_PATH = f'{ADV_MODEL_FOLDER}/{Config.ADV_GAN_CKPT}'
-
-        adv_model = AdvGAN.load_from_checkpoint(ADV_MODEL_PATH,
-                                                is_distilled=args.defense_is_distilled,
-                                                target_model_dir=TARGET_MODEL_PATH
-                                                )
+        adv_model = AdvGAN.load_from_checkpoint(adv_model_path)
 
         adv_model.freeze()
         adv_model.eval()
 
-    elif args.adv_model == 'fgsm':
-        adv_model = FGSM(target_model_dir=TARGET_MODEL_PATH)
+    elif adv_model_type == 'fgsm':
+        adv_model = FGSM(target_model_dir=target_model_folder)
 
-    elif args.adv_model == 'pgd':
-        adv_model = PGD(target_model_dir=TARGET_MODEL_PATH)
-
-    elif args.adv_model != '':
-        print("This attack is not implemented!")
-        quit()
-
-    eval_defense = not args.no_eval_defense
-    if eval_defense:
-        if args.def_model == 'ape_gan':
-            DEFENSE_MODEL_PATH = f'{DEFENSE_MODEL_FOLDER}/{Config.APE_GAN_CKPT}'
-            defense_model = ApeGan.load_from_checkpoint(DEFENSE_MODEL_PATH,
-                                                        in_ch=1,
-                                                        gen_loss_scale=Config.APE_GAN_gen_loss_scale,
-                                                        dis_loss_scale=Config.APE_GAN_dis_loss_scale,
-                                                        lr=Config.APE_GAN_lr,
-                                                        attack=adv_model,
-                                                        target_model_dir=TARGET_MODEL_PATH
-                                                        )
-        elif args.def_model == 'adv_gan_reverse':
-            DEFENSE_MODEL_PATH = f'{Config.LOGS_PATH}/adv_gan_reversed/{args.adv_model}{"-blackbox" if Config.IS_BLACK_BOX else "-whitebox"}{"-attack_is_distilled" if args.def_attack_is_distilled else ""}{"-defense_is_distilled" if args.defense_is_distilled else ""}/last.ckpt'
-            defense_model = AdvGANReverse.load_from_checkpoint(DEFENSE_MODEL_PATH,
-                                                        is_distilled=args.def_attack_is_distilled,
-                target_model_dir=TARGET_MODEL_PATH,
-                attack=adv_model
-            )
-
-    if args.dataset == 'mnist':
-        test_data = datasets.MNIST('mnist', train=False, download=True)
+    elif adv_model_type == 'pgd':
+        adv_model = PGD(target_model_dir=target_model_folder)
     else:
-        raise Exception("Other datasets are not supported yet")
-
-    PathCreator = CreatePaths(
-        adv_model=args.attack,
-        is_blackbox=Config.IS_BLACK_BOX,
-        is_distilled=args.attack_is_distilled)
-    TARGET_MODEL_PATH, ADV_MODEL_FOLDER, DEFENSE_MODEL_FOLDER_ATTACK = PathCreator.create_paths()
-
-    if args.attack == 'adv_gan':
-        ADV_MODEL_PATH = f'{ADV_MODEL_FOLDER}/{Config.ADV_GAN_CKPT}'
-
-        attack = AdvGAN.load_from_checkpoint(ADV_MODEL_PATH,
-                                             is_distilled=args.attack_is_distilled,
-                                             target_model_dir=TARGET_MODEL_PATH
-                                             )
-
-        attack.freeze()
-        attack.eval()
-
-    elif args.attack == 'fgsm':
-        attack = FGSM(target_model_dir=TARGET_MODEL_PATH)
-
-    elif args.attack == 'pgd':
-        attack = PGD(target_model_dir=TARGET_MODEL_PATH)
-
-    elif args.attack != '':
         print("This attack is not implemented!")
-        quit()
+        return None
+    return adv_model
 
-    X = test_data.data
-    X = X / 255
-    X = torch.unsqueeze(X, 1)
 
-    X_adv = attack(X)
+def load_defense(def_model_type, target_model_folder=None, defense_model_folder=None):
+    if def_model_type == 'ape_gan':
+        defense_model_path = f'{defense_model_folder}/{Config.APE_GAN_CKPT}'
+        defense_model = ApeGan.load_from_checkpoint(defense_model_path, strict=False)
+    elif def_model_type == 'adv_gan_reverse':
+        defense_model_path = f'{defense_model_folder}/last.ckpt'
+        defense_model = AdvGANReverse.load_from_checkpoint(defense_model_path)
+    else:
+        print("This attack is not implemented!")
+        return None
+    return defense_model
 
-    y = test_data.targets
-    if eval_defense:
-        X_res = defense_model(X_adv)
+
+def evaluate(X, y, attack_model, defense_model=None):
+    X_adv = attack_model(X)
 
     if not check_distance(X, X_adv, eps=args.eps):
         raise Exception("Adversarial samples has too large distance from original samples")
@@ -137,27 +68,73 @@ if __name__ == "__main__":
     robust_model.load_state_dict(torch.load(ROBUST_MODEL_PATH))
     robust_model.eval()
 
-    print(f"Adversarial examples are generated from {os.path.basename(os.path.normpath(DEFENSE_MODEL_FOLDER_ATTACK))}")
-    print(f"APE-GAN was trained on {os.path.basename(os.path.normpath(DEFENSE_MODEL_FOLDER))} \n")
+    with torch.no_grad():
+        probs = robust_model(X_adv)
+        pred = torch.argmax(probs, dim=1)
+        adv_accuracy = torch.sum(pred == y) / len(y)
+        print(f"Accuracy on adversarial samples is {adv_accuracy.item()}")
+
+        if defense_model is not None:
+            X_res = defense_model(X_adv)
+            probs = robust_model(X_res)
+            pred = torch.argmax(probs, dim=1)
+            res_accuracy = torch.sum(pred == y) / len(y)
+            print(f"Accuracy on restored samples is {res_accuracy.item()}")
+    return adv_accuracy, res_accuracy
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--adv-model", type=str, default='adv_gan')
+    parser.add_argument("--def-attack-model", type=str, default='adv_gan')
+    parser.add_argument("--def-model", type=str, default='ape_gan')
+    parser.add_argument("--no-eval-defense", default=False, action='store_true')
+
+    parser.add_argument("--attack-is-distilled", default=False, action='store_true')
+    parser.add_argument("--attack-is-blackbox", default=False, action='store_true')
+
+    parser.add_argument("--def-attack-is-distilled", default=False, action='store_true')
+    parser.add_argument("--def-attack-is-blackbox", default=False, action='store_true')
+
+    # currently not used TODO
+    parser.add_argument("--dataset", type=str, default='mnist')
+
+    parser.add_argument("--eps", type=float, default=0.3)
+    args = parser.parse_args()
+
+    adv_path_creator = CreatePaths(args.adv_model, args.attack_is_blackbox, args.attack_is_distilled)
+    target_model_path, adv_model_folder, _ = adv_path_creator.create_paths()
+    attack_model = load_attack(args.adv_model, adv_model_folder=adv_model_folder, target_model_folder=target_model_path)
+
+    eval_defense = not args.no_eval_defense
+    if eval_defense:
+        def_path_creator = CreatePaths(args.adv_model, args.def_attack_is_blackbox, args.def_attack_is_distilled)
+        _, _, defense_model_folder = def_path_creator.create_paths()
+        defense_model = load_defense(args.def_model, defense_model_folder=defense_model_folder)
+    else:
+        defense_model = None
+
+    if args.dataset == 'mnist':
+        test_data = datasets.MNIST('mnist', train=False, download=True)
+    else:
+        raise Exception("Other datasets are not supported yet")
+
+    X = test_data.data
+    X = X / 255
+    X = torch.unsqueeze(X, 1)
+    y = test_data.targets
+    adv_accuracy, res_accuracy = evaluate(X, y, attack_model, defense_model)
+
+    print(f"Adversarial examples are generated from {os.path.basename(os.path.normpath(adv_model_folder))}")
+    if eval_defense:
+        print(f"APE-GAN was trained on {os.path.basename(os.path.normpath(defense_model_folder))} \n")
 
     with open("evaluate_results.txt", "a") as file:
-        file.write(f'{os.path.basename(os.path.normpath(DEFENSE_MODEL_FOLDER_ATTACK))}\n')
-        file.write(f'{os.path.basename(os.path.normpath(DEFENSE_MODEL_FOLDER))}\n')
+        file.write(f'{os.path.basename(os.path.normpath(adv_model_folder))}\n')
+        file.write(f'{os.path.basename(os.path.normpath(defense_model_folder))}\n')
+        file.write(f'{adv_accuracy.item()}\n')
 
-        with torch.no_grad():
-            probs = robust_model(X_adv)
-            pred = torch.argmax(probs, dim=1)
-            
-            accuracy = torch.sum(pred == y) / len(y)
-            
-            print(f"Accuracy on adversarial samples is {accuracy.item()}")
-            file.write(f'{accuracy.item()}\n')
-
-            if eval_defense:
-                probs = robust_model(X_res)
-                pred = torch.argmax(probs, dim=1)
-                accuracy = torch.sum(pred == y) / len(y)
-                print(f"Accuracy on restored samples is {accuracy.item()}")
-                file.write(f'{accuracy.item()}\n')
+        if eval_defense:
+            file.write(f'{res_accuracy.item()}\n')
 
         file.write(f'\n')
